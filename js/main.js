e46e3e1db2a4d7c6f387dfd5888d352e5e2835b9
Sinf4er <39135955+Sinf4er@users.noreply.github.com>
2020-04-04 01:30:57 +0200

    origin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var opts = {
   method: "GET",
-  headers: {}
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET"
+  }
 };
 fetch(
   "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/hVnA4aaJn9mfQByRyY4EAbfb-tZiG9SM_pxG_oDJ-Qv8nfY?api_key=RGAPI-aae43291-7f23-4f90-ba62-46498a1bc200",
