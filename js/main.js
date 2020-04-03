1f4a9e207903efdbd52d6f4d2347cae23ca07adc
Sinf4er <39135955+Sinf4er@users.noreply.github.com>
2020-04-03 22:09:36 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var opts = {
+  method: "GET",
+  headers: {}
+};
+fetch(
+  "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/hVnA4aaJn9mfQByRyY4EAbfb-tZiG9SM_pxG_oDJ-Qv8nfY?api_key=RGAPI-aae43291-7f23-4f90-ba62-46498a1bc200",
+  opts
+)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(body) {
+    console.log(body);
+  });
