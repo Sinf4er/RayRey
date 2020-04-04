870b71b81f99ce873a338e47b8039ed993f25d4e
Sinf4er <39135955+Sinf4er@users.noreply.github.com>
2020-04-04 02:50:34 +0200

    origin

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 var opts = {
   method: "GET",
-  headers: {
-    "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, HEAD, OPTIONS",
-    "Access-Control-Allow-Origin": "https://sinf4er.github.io",
-    "Access-Control-Allow-Headers":
-      "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-  }
+  headers: {}
 };
 fetch(
   "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/hVnA4aaJn9mfQByRyY4EAbfb-tZiG9SM_pxG_oDJ-Qv8nfY?api_key=RGAPI-aae43291-7f23-4f90-ba62-46498a1bc200",
