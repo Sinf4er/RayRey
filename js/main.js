48ea88a09b5d95b95cd71f96c32555eb6905bcb5
Sinf4er <39135955+Sinf4er@users.noreply.github.com>
2020-04-05 00:23:26 +0200

    Add twitch videos & banner & footer

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-var opts = {
-  method: "post",
-  headers: {}
+var Riotopts = {
+  method: "GET",
+  headers: {},
 };
 fetch(
-  "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/hVnA4aaJn9mfQByRyY4EAbfb-tZiG9SM_pxG_oDJ-Qv8nfY?api_key=RGAPI-aae43291-7f23-4f90-ba62-46498a1bc200",
-  opts
+  "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/hVnA4aaJn9mfQByRyY4EAbfb-tZiG9SM_pxG_oDJ-Qv8nfY?api_key=RGAPI-81dbd514-27ea-4eaa-9128-2f16562d429d",
+  Riotopts
 )
-  .then(function(response) {
+  .then(function (response) {
     return response.json();
   })
-  .then(function(body) {
+  .then(function (body) {
     console.log(body);
   });
+  
