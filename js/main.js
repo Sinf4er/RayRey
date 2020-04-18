b7589c1e410929e3add8589575708130847dc202
Sinf4er <39135955+Sinf4er@users.noreply.github.com>
2020-04-19 00:12:23 +0200

    Responsive & little changes

--- js/main.js
+++ js/main.js
@@ -12,3 +12,21 @@ document.querySelector(".to-top").addEventListener("click", (_e) => {
   _e.preventDefault();
   TweenLite.to(window, 1, { scrollTo: ".header-section" });
 });
+
+const menu = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-nav");
+const body = document.querySelector("body");
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  mobileMenu.classList.toggle("is-open");
+  body.classList.toggle("fixed");
+});
+
+document.querySelector(".to-about3").addEventListener("click", (_e) => {
+  _e.preventDefault();
+  TweenLite.to(window, 1, { scrollTo: ".about-section" });
+  menu.classList.toggle("is-active");
+  body.classList.toggle("fixed");
+  mobileMenu.classList.toggle("is-open");
+});
